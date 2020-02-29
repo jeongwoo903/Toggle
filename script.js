3821898d4ea47ecadbae68cda06a5cfe52ef52ca
jeongwoo903 <www8565@naver.com>
2020-02-29 21:03:56 +0900

    Initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+// const outline = document.querySelector(".outline");
+
+// outline.addEventListener("click", function (e) {
+//     document.querySelector(".toggle").classList.toggle("trans");
+//     e.target.classList.toggle("trans");
+// })
