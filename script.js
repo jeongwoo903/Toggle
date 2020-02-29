a8bf7271b492c102b0b205ed060418d0717dc1fe
jeongwoo903 <www8565@naver.com>
2020-02-29 21:04:40 +0900

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-// const outline = document.querySelector(".outline");
-
-// outline.addEventListener("click", function (e) {
-//     document.querySelector(".toggle").classList.toggle("trans");
-//     e.target.classList.toggle("trans");
-// })
